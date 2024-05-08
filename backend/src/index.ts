1ec1f8841108b55f46dbd47ad4e37f34925416d3
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2024-05-08 18:29:09 +0530

    update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,12 +14,16 @@ import openai from "./openAI";
 // Variables
 const app: Express = express();
 const server = createServer(app);
+const port = process.env.PORT || 8081;
 const io: ServerType = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000'
+    origin: (origin: string, callback: any) => {
+      if (["http://localhost:3000", "https://all4one.vercel.app"].includes(origin)) {
+        callback(null, origin);
+      }
+    }
   }
 });
-const port = process.env.PORT || 8081;
 
 app.use(cors())
 
@@ -30,51 +34,51 @@ app.get('/', (req: Request, res: Response) => {
 
 io.on('connection', (socket: Socket) => {
 
-    socket.on('join-call', (data : { id : string }) => {
-      socket.join(data.id)
+  socket.on('join-call', (data: { id: string }) => {
+    socket.join(data.id)
+  })
+
+  socket.on('send-audio', async (data: any) => {
+
+    console.log("Received audio")
+
+    const savedAudio = await saveRecording(data.audio, socket.id, "audio")
+
+    // testing
+    if (data.audio === "test")
+      savedAudio.folderPath = path.join(__dirname, "../", "audio", "test audio.mp3")
+    console.log(savedAudio)
+
+
+    const translation = await openai.audio.translations.create({
+      file: fs.createReadStream(savedAudio.folderPath),
+      model: 'whisper-1'
     })
 
-    socket.on('send-audio', async (data: any) => {
+    console.log("Translation successeded")
 
-      console.log("Received audio")
-
-      const savedAudio = await saveRecording(data.audio, socket.id, "audio")
-
-      // testing
-      if(data.audio === "test")
-        savedAudio.folderPath = path.join(__dirname, "../" , "audio", "test audio.mp3")
-      console.log(savedAudio)
-
-
-      const translation = await openai.audio.translations.create({
-        file: fs.createReadStream(savedAudio.folderPath),
-        model: 'whisper-1'
-      })
-
-      console.log("Translation successeded")
-
-      const voice = await openai.audio.speech.create({
-        model: 'tts-1',
-        voice: 'nova',
-        input: translation.text
-      })
-
-      console.log("TTS completed")
-
-      const buffer = Buffer.from(await voice.arrayBuffer())
-      let filePathOutput = await saveRecording(buffer, socket.id + "output", "outputs")
-
-      console.log("Saved output")
-      
-      socket.broadcast.to(data.callID).emit("receive-translation", { "filePath" : [filePathOutput.fileName] })
-
+    const voice = await openai.audio.speech.create({
+      model: 'tts-1',
+      voice: 'nova',
+      input: translation.text
     })
-    
+
+    console.log("TTS completed")
+
+    const buffer = Buffer.from(await voice.arrayBuffer())
+    let filePathOutput = await saveRecording(buffer, socket.id + "output", "outputs")
+
+    console.log("Saved output")
+
+    socket.broadcast.to(data.callID).emit("receive-translation", { "filePath": [filePathOutput.fileName] })
+
+  })
+
 });
 
 
-app.get('/audio/outputs/:file', function(req, res){
-  const file = path.join(__dirname, "../", "outputs" , req.params.file);
+app.get('/audio/outputs/:file', function (req, res) {
+  const file = path.join(__dirname, "../", "outputs", req.params.file);
   res.download(file);
 });
 
