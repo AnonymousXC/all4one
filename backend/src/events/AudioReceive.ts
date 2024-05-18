250e504317b9050caceeed702428590131367692
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2024-05-18 19:32:39 +0530

    updated file saving

--- backend/src/events/AudioReceive.ts
+++ backend/src/events/AudioReceive.ts
@@ -3,9 +3,83 @@ import saveRecording from "../helpers/audioBlobToFile"
 import openai from "../openAI";
 import { io } from "..";
 import { emailLanguageMap } from "./LanguageSetter";
-const path = require('path')
-const fs = require('fs')
+import { toFile } from "openai";
+import getTimeNow from "../helpers/getTime";
 
+const sentAudio = async function(this: Socket, data: any) {
+
+    const socket = this;
+
+    try {
+      var users = io.sockets.adapter.rooms.get(data.callID)
+      var endUserID = ""
+      users?.forEach(val => {
+        if(val !== socket.id)
+          endUserID = val
+      })
+      // @ts-expect-error
+      let endUserLanguage = emailLanguageMap[endUserID]
+      if(endUserLanguage === undefined || !endUserLanguage)
+        endUserLanguage = "ENGLISH"
+
+      console.log("Received audio", endUserLanguage)
+
+      const translation = await openai.audio.transcriptions.create({
+        file: await toFile(data.audio, socket.id + getTimeNow() + ".wav"),
+        model: 'whisper-1',
+        response_format: 'json'
+      })
+
+
+      console.log("Transcription successeded : " + translation.text)
+
+      // Translate text to any language
+      
+      console.log("GPT 3.5 Translation begins")
+      const getTranslationToLang = await openai.chat.completions.create(
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {
+              role: 'user',
+              content : `Translate ${translation.text} to ${endUserLanguage}`
+            }
+          ]
+        }
+      )
+
+      const translatedText = getTranslationToLang.choices[0].message.content
+      console.log("GPT 3.5 Translation ended")
+
+      const voice = await openai.audio.speech.create({
+        model: 'tts-1',
+        voice: 'nova',
+        input: translatedText || ''
+      })
+
+
+      console.log("TTS completed")
+
+
+      const buffer = Buffer.from(await voice.arrayBuffer())
+      let filePathOutput = await saveRecording(buffer, socket.id + "output", "outputs")
+
+
+      console.log(filePathOutput)
+      console.log("Saved output")
+
+      
+      socket.broadcast.to(data.callID).emit("receive-translation", { "filePath": filePathOutput.fileName, "text": translatedText })
+    }
+    catch (err) {
+      socket.emit("error processing audio", { text : "Error in processing."})
+      console.log(err)
+    }
+}
+
+
+/**
+ * 
 const sentAudio = async function(this: Socket, data: any) {
 
     const socket = this;
@@ -21,21 +95,21 @@ const sentAudio = async function(this: Socket, data: any) {
 
     console.log("Received audio")
 
-    const savedAudio = await saveRecording(data.audio, socket.id, "audio")
+    // const savedAudio = await saveRecording(data.audio, socket.id, "audio")
 
     // testing
-    if (data.audio === "test")
-      savedAudio.folderPath = path.join(__dirname, "../", "audio", "test audio.mp3")
-    console.log(savedAudio)
+    // if (data.audio === "test")
+    //   savedAudio.folderPath = path.join(__dirname, "../", "audio", "test audio.mp3")
+    // console.log(savedAudio)
 
 
     const translation = await openai.audio.translations.create({
-      file: fs.createReadStream(savedAudio.folderPath),
-      model: 'whisper-1'
+      file: await toFile(data.audio, socket.id + getTimeNow() + ".wav"),
+      model: 'whisper-1',
     })
 
 
-    console.log("Translation successeded")
+    console.log("Translation successeded : " + translation.text)
 
     // Translate text to any language
     
@@ -76,53 +150,6 @@ const sentAudio = async function(this: Socket, data: any) {
     socket.broadcast.to(data.callID).emit("receive-translation", { "filePath": filePathOutput.fileName, "text": translatedText })
 
 }
-
-
-/**
- * 
-const sentAudio = async function(this: Socket, data: any) {
-
-    const socket = this;
-
-    console.log("Received audio")
-
-    const savedAudio = await saveRecording(data.audio, socket.id, "audio")
-
-    // testing
-    if (data.audio === "test")
-      savedAudio.folderPath = path.join(__dirname, "../", "audio", "test audio.mp3")
-    console.log(savedAudio)
-
-
-    const translation = await openai.audio.translations.create({
-      file: fs.createReadStream(savedAudio.folderPath),
-      model: 'whisper-1'
-    })
-
-
-    console.log("Translation successeded")
-
-
-    const voice = await openai.audio.speech.create({
-      model: 'tts-1',
-      voice: 'nova',
-      input: translation.text
-    })
-
-
-    console.log("TTS completed")
-
-
-    const buffer = Buffer.from(await voice.arrayBuffer())
-    let filePathOutput = await saveRecording(buffer, socket.id + "output", "outputs")
-
-
-    console.log(filePathOutput)
-    console.log("Saved output")
-
-    
-    socket.broadcast.to(data.callID).emit("receive-translation", { "filePath": filePathOutput.fileName, "text": translation.text })
-
 }
  */
 
